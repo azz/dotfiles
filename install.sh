34a496cd8a061a7e91f9c8fb5b0990276b245ab8
Lucas Azzola <derflatulator@gmail.com>
2019-01-09 21:26:38 +1100

    install.sh: configure ssh, gpg, git

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash +x
+#!/usr/bin/env bash +xu
+
+: "${EMAIL}"
 
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -23,8 +25,8 @@ ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+# Install GNU `sed`, NOT overwriting the built-in `sed`.
+brew install gnu-sed # --with-default-names
 # Install Bash 4.
 brew install bash
 brew install bash-completion2
@@ -105,6 +107,7 @@ brew install mkcert
 brew install mercurial
 brew install graphviz
 brew install the_silver_searcher
+brew install autojump
 
 # Install Node.js
 brew install nvm
@@ -124,15 +127,16 @@ brew cask install gimp
 brew cask install google-chrome
 brew cask install homebrew/cask-versions/google-chrome-canary
 brew cask install homebrew/cask-versions/microsoft-remote-desktop-beta
+brew cask install keybase
 brew cask install slack
 brew cask install spotify
 brew cask install vlc
-brew cask install wireshark
 brew cask install visual-studio-code
-brew cask install zoomus
+brew cask install wireshark
 brew cask install virtualbox
 brew cask install vagrant
 brew cask install vagrant-manager
+brew cask install zoomus
 
 function add_app_to_dock {
   defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$APP_PATH</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
@@ -144,6 +148,7 @@ add_app_to_dock "/Applications/Slack.app"
 add_app_to_dock "/Applications/zoom.us.app"
 add_app_to_dock "/Applications/Visual Studio Code.app"
 add_app_to_dock "/Applications/Spotify.app"
+add_app_to_dock "/Applications/Keybase.app"
 add_app_to_dock "/Applications/Utilities/Terminal.app"
 add_app_to_dock "/Applications/Utilities/Activity Monitor.app"
 killall Dock
@@ -169,5 +174,38 @@ yarn global add trash-cli
 yarn global add is-up-cli
 yarn global add is-online-cli
 
+# Generate an ~/.ssh/id_rsa key-pair
+if [ ! -f ~/.ssh/id_rsa ]; then
+  ssh-keygen -t rsa -C "$EMAIL" -P '' -f ~/.ssh/id_rsa 
+fi
+
+# Set up GPG key
+cat <<EOF > /tmp/gpg-script
+Key-Type: 1
+Key-Length: 4096
+Subkey-Type: 1
+Subkey-Length: 4096
+Name-Real: Lucas Azzola
+Name-Email: $EMAIL
+Expire-Date: 0
+EOF
+gpg --batch --gen-key /tmp/gpg-script
+mkdir -p ~/.gpg
+gpg --armor --export "$EMAIL" > "~/.gpg/$EMAIL.asc"
+GPG_KEY_ID=$(gpg --list-keys --with-colons "$EMAIL" | awk -F: '/^pub:/ { print $5 }')
+
+# Configure git
+git config --global user.signingkey "$GPG_KEY_ID"
+git config --global commit.gpgsign true
+git config --global user.name "Lucas Azzola"
+git config --global user.email "$EMAIL"
+
+# Make some directories
+mkdir -p ~/code/azz
+
 # Start any boxes in ~/Vagrantfile
 vagrant up
+
+echo ""
+echo "ALL DONE!"
+echo ""
