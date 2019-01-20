913c6458220e321a240b6c1c13ea45ecf1c3e743
Lucas Azzola <derflatulator@gmail.com>
2019-01-20 12:38:38 +1100

    install.sh: fix iTerm.app to dock

--- install.sh
+++ install.sh
@@ -148,7 +148,7 @@ function add_app_to_dock {
   defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$APP_PATH</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 }
 
-add_app_to_dock "/Applications/iTerm2.app"
+add_app_to_dock "/Applications/iTerm.app"
 add_app_to_dock "/Applications/Utilities/Terminal.app"
 add_app_to_dock "/Applications/Utilities/Activity Monitor.app"
 add_app_to_dock "/Applications/Visual Studio Code.app"
