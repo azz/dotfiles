54e44c6337a09733675bd81333f77ad1f4e71c37
Lucas Azzola <lucas.azzola@ferocia.com.au>
2025-09-10 22:48:17 +1000

    Big prune

--- Vagrantfile
+++
@@ -1,7 +0,0 @@
-Vagrant.configure("2") do |config|
-  config.vm.box = "Microsoft/EdgeOnWindows10"
-  config.vm.box_version = "1.0"
-  config.vm.provider "virtualbox" do |virtualbox|
-    virtualbox.gui = true
-  end
-end
--- install.sh
+++ install.sh
@@ -43,131 +43,46 @@ fi;
 # Install `wget` with IRI support.
 brew install wget --with-iri
 
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
-brew install screen
-brew install php
+brew install tmux
 brew install gmp
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
 brew install nmap
-brew install pngcheck
-brew install socat
 brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
 brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
 
 # Install other useful binaries.
 brew install ack
-#brew install exiv2
 brew install git
 brew install git-lfs
 brew install imagemagick --with-webp
-brew install lua
 brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
 brew install tree
-brew install vbindiff
-brew install zopfli
 brew install htop
-brew install hub
+brew install btop
 brew install httpie
 brew install mkcert
-brew install mercurial
 brew install graphviz
 brew install the_silver_searcher
 brew install autojump
-brew install youtube-dl
 
-# Install Node.js
-brew install nvm
-nvm install --lts
-brew install yarn --without-node
-
-# brew cask applications
-brew tap caskroom/cask
-
-# Install Docker
-brew cask install docker # Docker Community Edition for macOS
-# brew install docker-compose
-
-brew cask install discord
-brew cask install kap
-brew cask install gimp
-brew cask install google-chrome
-brew cask install homebrew/cask-versions/google-chrome-canary
-brew cask install homebrew/cask-versions/microsoft-remote-desktop-beta
-brew cask install iterm2
-brew cask install keybase
-brew cask install postman
-brew cask install slack
-brew cask install spectacle
-brew cask install spotify
-brew cask install vlc
-brew cask install visual-studio-code
-brew cask install wireshark
-brew cask install virtualbox
-brew cask install vagrant
-brew cask install vagrant-manager
-brew cask install zoomus
-
-# Install https://github.com/sindresorhus/quick-look-plugins
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json webpquicklook suspicious-package quicklookase qlvideo
+# Install Casks
+brew install --cask 1password
+brew install --cask rancher
+brew install --cask ghostty
+brew install --cask raycast
+brew install --cask slack
+brew install --cask visual-studio-code
+brew install --cask zed
+brew install --cask zoomus
 
 # Install displaylink
-brew tap homebrew/cask-drivers
-brew cask install displaylink
-
-function add_app_to_dock {
-  defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$APP_PATH</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-}
-
-add_app_to_dock "/Applications/iTerm.app"
-add_app_to_dock "/Applications/Utilities/Terminal.app"
-add_app_to_dock "/Applications/Utilities/Activity Monitor.app"
-add_app_to_dock "/Applications/Visual Studio Code.app"
-# add_app_to_dock "/Applications/Google Chrome.app"
-add_app_to_dock "/Applications/Google Chrome Canary.app"
-add_app_to_dock "/Applications/Slack.app"
-add_app_to_dock "/Applications/zoom.us.app"
-add_app_to_dock "/Applications/Postman.app"
-add_app_to_dock "/Applications/Spotify.app"
-add_app_to_dock "/Applications/Keybase.app"
-killall Dock
+# brew tap homebrew/cask-drivers
+# brew cask install displaylink
 
 # Install XCode for make and friends
 hash make || xcode-select --install
@@ -175,45 +90,23 @@ hash make || xcode-select --install
 # Remove outdated versions from the cellar.
 brew cleanup
 
-# Install nifty npm tools
-
-yarn global add yo
-yarn global add http-server
-yarn global add tldr
-yarn global add fkill
-yarn global add castnow
-yarn global add vtop
-yarn global add create-react-app
-yarn global add ndb
-yarn global add speed-test
-yarn global add trash-cli
-yarn global add is-up-cli
-yarn global add is-online-cli
-
-# Generate an ~/.ssh/id_rsa key-pair
-if [ ! -f ~/.ssh/id_rsa ]; then
-  ssh-keygen -t rsa -C "$EMAIL" -P '' -f ~/.ssh/id_rsa 
-fi
-
 # Allow authentication of 'sudo' commands with Touch ID
-if [ $(cat /etc/pam.d/sudo | grep pam_tid | wc -l) = '0' ]
-then
+if [ $(cat /etc/pam.d/sudo | grep pam_tid | wc -l) = '0' ]; then
   sudo gsed -i '2iauth sufficient pam_tid.so' /etc/pam.d/sudo
 fi
 
 # Configure git
-git config --global user.signingkey "$GPG_KEY_ID"
-git config --global commit.gpgsign true
 git config --global user.name "Lucas Azzola"
 git config --global user.email "$EMAIL"
 
 # Make some directories
-mkdir -p ~/code/azz
+mkdir -p ~/src
 mkdir -p ~/Downloads/Installers
 mkdir -p ~/Downloads/Artifacts
 
-# Start any boxes in ~/Vagrantfile
-vagrant up
+# Install oh-my-tmux
+curl -fsSL "https://github.com/gpakosz/.tmux/raw/refs/heads/master/install.sh#$(date +%s)" | bash
+
 
 echo ""
 echo "ALL DONE!"
